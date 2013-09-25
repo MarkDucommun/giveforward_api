1544e9971cf18c26b417b15b27fe827ad6aafd0e
MarkDucommun <ducommun.mark@gmail.com>
2013-09-25 16:10:56 -0500

    recreates normal show functionality for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     if user = User.find_by_token(params[:token])
       render json: { user: user.to_json }
     else
-
+      render :show
     end
   end
 
